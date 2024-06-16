045441e8cfcc4bf421100918030e7c1742ff2493
Ryel Banfield <ryelbanfield@gmail.com>
2024-06-16 00:55:29 -0400

    Finish Switch

---
+++ src/App.tsx
@@ -0,0 +1,166 @@
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { clsx } from "clsx";
+
+const App = () => {
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className="grid h-screen w-screen place-items-center bg-neutral-950">
+      <motion.div
+        onClick={() => setDarkMode(!isDarkMode)}
+        className={clsx(
+          "relative flex h-[80px] w-[240px] items-center overflow-hidden rounded-full p-[8px] transition-all duration-500",
+          {
+            "justify-start": !isDarkMode,
+            "justify-end": isDarkMode,
+          }
+        )}
+      >
+        <AnimatePresence>
+          {!isDarkMode ? (
+            <motion.div
+              key={1}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0.5 }}
+              transition={{ duration: 0.5 }}
+              className="absolute inset-0 h-full w-full justify-start rounded-full bg-gradient-to-r from-sky-500 to-sky-100"
+            >
+              <div className="absolute bottom-[45px] right-[-20px] aspect-square h-[40px] w-[40px] rounded-full bg-white" />
+              <div className="absolute bottom-[20px] right-[0px] aspect-square h-[40px] w-[40px] rounded-full bg-white" />
+              <div className="absolute bottom-[10px] right-[0px] aspect-square h-[40px] w-[40px] rounded-full bg-white" />
+              <div className="absolute bottom-[0px] right-[0px] aspect-square h-[40px] w-[40px] rounded-full bg-white" />
+              <div className="absolute bottom-[0px] right-[20px] aspect-square h-[40px] w-[40px] rounded-full bg-white" />
+              <div className="absolute bottom-[-10px] right-[40px] aspect-square h-[40px] w-[40px] rounded-full bg-white" />
+              <div className="absolute bottom-[-20px] right-[60px] aspect-square h-[40px] w-[40px] rounded-full bg-white" />
+            </motion.div>
+          ) : (
+            <motion.div
+              key={2}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0.5 }}
+              transition={{ duration: 0.5 }}
+              className="absolute inset-0 h-full w-full justify-end rounded-full bg-gradient-to-r from-gray-900 to-gray-500"
+            >
+              <div className="absolute left-[15px] top-[22px] text-neutral-100">
+                <svg
+                  width="8"
+                  height="8"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+
+              <div className="absolute left-[37px] top-[12px] text-neutral-100">
+                <svg
+                  width="4"
+                  height="4"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+
+              <div className="absolute left-[35px] top-[35px] text-neutral-100">
+                <svg
+                  width="6"
+                  height="6"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+
+              <div className="absolute bottom-[28px] left-[12px] text-neutral-100">
+                <svg
+                  width="6"
+                  height="6"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+
+              <div className="absolute bottom-[14px] left-[30px] text-neutral-100">
+                <svg
+                  width="3"
+                  height="3"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+
+              <div className="absolute bottom-[18px] left-[50px] text-neutral-100">
+                <svg
+                  width="7"
+                  height="7"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+
+              <div className="absolute bottom-[50px] left-[60px] text-neutral-100">
+                <svg
+                  width="5"
+                  height="5"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7.22303 0.665992C7.32551 0.419604 7.67454 0.419604 7.77702 0.665992L9.41343 4.60039C9.45663 4.70426 9.55432 4.77523 9.66645 4.78422L13.914 5.12475C14.18 5.14607 14.2878 5.47802 14.0852 5.65162L10.849 8.42374C10.7636 8.49692 10.7263 8.61176 10.7524 8.72118L11.7411 12.866C11.803 13.1256 11.5206 13.3308 11.2929 13.1917L7.6564 10.9705C7.5604 10.9119 7.43965 10.9119 7.34365 10.9705L3.70718 13.1917C3.47945 13.3308 3.19708 13.1256 3.25899 12.866L4.24769 8.72118C4.2738 8.61176 4.23648 8.49692 4.15105 8.42374L0.914889 5.65162C0.712228 5.47802 0.820086 5.14607 1.08608 5.12475L5.3336 4.78422C5.44573 4.77523 5.54342 4.70426 5.58662 4.60039L7.22303 0.665992Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+
+        <motion.div
+          layout
+          animate={{ backgroundColor: isDarkMode ? "#d1d5db" : "#fde047" }}
+          transition={{ duration: 0.5 }}
+          className="combined-shadow z-50 aspect-square h-full rounded-full"
+        />
+      </motion.div>
+    </div>
+  );
+};
+
+export default App;
